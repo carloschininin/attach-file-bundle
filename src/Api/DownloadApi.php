35df7317f7dd32ead7aa761ba01b20c5167e9460
Carlos Chininin <carloschininin@gmail.com>
2023-10-21 12:34:20 -0500

    add link download attach file

---
+++ src/Api/DownloadApi.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA.
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\AttachFile\Api;
+
+use CarlosChininin\AttachFile\Model\AttachFile;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+final class DownloadApi
+{
+    public function __construct(
+        private readonly ParameterBagInterface $parameterBag
+    ) {
+    }
+
+    public function __invoke(AttachFile $attachFile): Response
+    {
+        $publicDirectory = $this->parameterBag->get('app.public_directory');
+        $attachDirectory = $this->parameterBag->get('app.attach_file_directory');
+        $filePath = $publicDirectory.$attachDirectory.$attachFile->filePath();
+        $response = new BinaryFileResponse($filePath);
+        $response->trustXSendfileTypeHeader();
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $attachFile->name()
+        );
+
+        return $response;
+    }
+}
