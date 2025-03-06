763506094f6d36f28d0eec1a8c0dfdc035337be3
Carlos Chininin <carloschininin@gmail.com>
2025-03-06 00:22:13 -0500

    fix download attach file

--- src/Api/DownloadApi.php
+++ src/Api/DownloadApi.php
@@ -11,6 +11,7 @@ namespace CarlosChininin\AttachFile\Api;
 
 use CarlosChininin\AttachFile\Exception\FileNotFoundException;
 use CarlosChininin\AttachFile\Model\AttachFile;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,11 +20,17 @@ final readonly class DownloadApi
 {
     public function __construct(
         private ParameterBagInterface $parameterBag,
+        private EntityManagerInterface $entityManager,
     ) {
     }
 
-    public function __invoke(AttachFile $attachFile, ?string $disposition = null): Response
+    public function __invoke(string $secure, ?string $disposition = null): Response
     {
+        $attachFile = $this->entityManager->getRepository(AttachFile::class)->findOneBy(['secure' => $secure]);
+        if (null === $attachFile) {
+            throw new \RuntimeException(\sprintf('The file %s not exist in database', $secure));
+        }
+
         $publicDirectory = $this->parameterBag->get('app.public_directory');
         $filePath = $publicDirectory.$attachFile->filePath();
         if (!file_exists($filePath)) {
